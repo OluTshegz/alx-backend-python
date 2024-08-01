a9b2298d3d18d423bcc10655024f31731d6996ca
Olusegun <ayosegun2001@gmail.com>
2024-08-02 00:27:38 +0100

    0x00. Python - Variable Annotations: Python_Back-end

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -7,7 +7,7 @@ into a tuple by repeating its elements.
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Returns a list with elements from the
     tuple repeated according to the factor.
@@ -21,7 +21,7 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List[int]:
     Returns:
     List[int]: A list with repeated elements.
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
