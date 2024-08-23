bea7d9c5728cc0532c17150179d4679280e71df4
Olusegun <ayosegun2001@gmail.com>
2024-08-23 21:14:27 +0100

    0x03. Unittests and Integration Tests: Unit Tests, Back-end, Integration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -13,7 +13,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map returns expected output"""
@@ -21,7 +21,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),
+        ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """Test access_nested_map raises KeyError for invalid paths"""
